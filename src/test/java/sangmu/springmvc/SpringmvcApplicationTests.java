493bbee99f8912f31794d3caf4f2fd141a47811b
leessang10 <leessang10@naver.com>
2022-05-10 16:12:27 +0900

    Initial commit

---
+++ src/test/java/sangmu/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package sangmu.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
